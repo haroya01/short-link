8d0918093f8c47366b09caf37722bba7d2e48235
Haroya <128161745+haroya01@users.noreply.github.com>
2026-05-04 23:28:57 +0900

    chore: setup dev env with asdf, spotless, lefthook (#2)

--- src/test/java/com/example/short_link/ShortLinkApplicationTests.java
+++ src/test/java/com/example/short_link/ShortLinkApplicationTests.java
@@ -6,8 +6,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class ShortLinkApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
-
+  @Test
+  void contextLoads() {}
 }
