d616a4df9eea0759128876b35417d3e3f2cbc509
Haroya <128161745+haroya01@users.noreply.github.com>
2026-05-04 23:43:05 +0900

    chore: setup flyway with mysql migrations (#5)

--- src/test/java/com/example/short_link/ShortLinkApplicationTests.java
+++ src/test/java/com/example/short_link/ShortLinkApplicationTests.java
@@ -2,8 +2,10 @@ package com.example.short_link;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class ShortLinkApplicationTests {
 
   @Test
