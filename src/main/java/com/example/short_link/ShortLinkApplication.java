23913186935bc210bac7de87dfc439514f49c41d
gimdonghyeon <haroya01@naver.com>
2026-05-07 00:02:43 +0900

    feat: add redirect endpoint with redis caching

--- src/main/java/com/example/short_link/ShortLinkApplication.java
+++ src/main/java/com/example/short_link/ShortLinkApplication.java
@@ -2,8 +2,10 @@ package com.example.short_link;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching
 public class ShortLinkApplication {
 
   public static void main(String[] args) {
