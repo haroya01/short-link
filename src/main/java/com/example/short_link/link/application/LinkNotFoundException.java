fb32123af7921b6c0c7db25b7dce1bdd13d3f839
Haroya <128161745+haroya01@users.noreply.github.com>
2026-05-07 00:13:45 +0900

    chore: standardize error responses with problemdetail (#25)

--- src/main/java/com/example/short_link/link/application/LinkNotFoundException.java
+++ src/main/java/com/example/short_link/link/application/LinkNotFoundException.java
@@ -1,9 +1,5 @@
 package com.example.short_link.link.application;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ResponseStatus;
-
-@ResponseStatus(HttpStatus.NOT_FOUND)
 public class LinkNotFoundException extends RuntimeException {
 
   public LinkNotFoundException(String shortCode) {
