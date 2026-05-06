6a69518117e78c46d5b606baf756f927086f6de9
Haroya <128161745+haroya01@users.noreply.github.com>
2026-05-07 04:34:13 +0900

    feat: 악성/피싱 URL 차단 (Google Safe Browsing) (#36)

--- src/main/java/com/example/short_link/ShortLinkApplication.java
+++ src/main/java/com/example/short_link/ShortLinkApplication.java
@@ -2,10 +2,12 @@ package com.example.short_link;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
 @EnableCaching
+@ConfigurationPropertiesScan
 public class ShortLinkApplication {
 
   public static void main(String[] args) {
